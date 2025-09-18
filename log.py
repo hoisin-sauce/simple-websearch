d027e019d4672c9f84236f38debe322c4c0292f7
hoisin-sauce <97987829+hoisin-sauce@users.noreply.github.com>
2025-09-18 11:52:02 +0100

    Reapply "Moved all database calls into a single thread to manage concurrency issues"
    This reverts commit fae3a4f74f84fcf855476f4ec38c16217241ac41.

--- log.py
+++ log.py
@@ -1,2 +1,10 @@
+def do_nothing(*args, **kwargs):
+    del args, kwargs # ignore for reason
+    pass
+
 # TODO not be lazy and write logging properly
-log = print
+# log = print # NOLOG
+log = do_nothing
+
+# TODO implement thread tracing (maybe in a database?) based on threading.setprofile
+
